4482cc9b2bbf4550aa8d593b51b3f16683166e9c
sowmiya-projects <sowmithanam03@gmail.com>
2026-02-23 11:27:03 +0530

    Update hello.rs

--- api/hello.rs
+++ api/hello.rs
@@ -1,12 +1,14 @@
-use vercel_runtime::{run, Body, Error, Request, Response};
+use vercel_runtime::{run, Body, Error, Request, Response, service_fn};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let service = service_fn(handler);
+    run(service).await
+}
 
 async fn handler(_req: Request) -> Result<Response<Body>, Error> {
     Ok(Response::builder()
         .status(200)
         .header("content-type", "text/plain")
         .body("Hello Sowmiya 🚀 Rust deployed successfully!".into())?)
-}
-
-fn main() -> Result<(), Error> {
-    run(handler)
 }
